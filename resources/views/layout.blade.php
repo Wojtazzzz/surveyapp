011c5b67e222569e7750ea67d220382c7c7b8ce6
Marcin Witas <77293179+Wojtazzzz@users.noreply.github.com>
2023-01-30 19:24:11 +0100

    chore/add tailwindcss

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Survey App</title>
+    @vite('resources/css/tailwindcss.css')
+</head>
+
+<body>
+    <span class="text-4xl text-red-800">Hello World!</span>
+</body>
+
+</html>
