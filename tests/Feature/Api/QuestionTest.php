c0a730c41f6f4804565bd2ac7ca0c0fa60cab2dc
Marcin Witas <77293179+Wojtazzzz@users.noreply.github.com>
2023-02-05 22:15:49 +0100

    test(api)/add survey tests

---
+++ tests/Feature/Api/QuestionTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace Tests\Feature\Api;
+
+use App\Enums\SurveyStatus;
+use App\Models\Survey;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class QuestionTest extends TestCase
+{
+    public function test_return_not_found_when_model_not_exists(): void
+    {
+        $response = $this->getJson('/survey/1');
+
+        $response->assertNotFound();
+    }
+
+    public function test_return_success_when_model_exists(): void
+    {
+        $survey = Survey::factory()->createOne([
+            'status' => SurveyStatus::READY
+        ]);
+
+        $response = $this->getJson(route('api.v1.surveys.show', ['survey' => $survey]));
+
+        $response->assertSuccessful();
+    }
+
+    public function test_return_correct_data(): void
+    {
+        $survey = Survey::factory()->createOne([
+            'status' => SurveyStatus::READY
+        ]);
+
+        $response = $this->getJson(route('api.v1.surveys.show', ['survey' => $survey]));
+
+        $response->assertJson(fn (AssertableJson $json) =>
+            $json->where('id', $survey->id)
+                ->where('name', $survey->name)
+                ->where('status', SurveyStatus::READY->value)
+                ->where('created', $survey->created_at->toDateString())
+                ->where('updated', $survey->updated_at->toDateString())
+                ->etc()
+        );
+    }
+
+    public function test_return_not_found_when_survey_has_editing_status(): void
+    {
+        $survey = Survey::factory()->createOne([
+            'status' => SurveyStatus::EDITING
+        ]);
+
+        $response = $this->getJson(route('api.v1.surveys.show', ['survey' => $survey]));
+
+        $response->assertNotFound();
+    }
+
+    public function test_return_not_found_when_survey_has_testing_status(): void
+    {
+        $survey = Survey::factory()->createOne([
+            'status' => SurveyStatus::TESTING
+        ]);
+
+        $response = $this->getJson(route('api.v1.surveys.show', ['survey' => $survey]));
+
+        $response->assertNotFound();
+    }
+}
